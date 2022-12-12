8a063e5b972497db87ffb87076303b55b5e721cd
yasuhironagaya <69679135+yasuhironagaya@users.noreply.github.com>
2022-12-12 21:54:29 +0900

    💄 動作可能

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,47 @@ import Form from "./components/Form";
 import Results from "./components/Results";
 import "./App.css";
 
-
+type ResultsStateType = {
+  country: string;
+  cityName: string;
+  temperature: string;
+  conditionText: string;
+  icon: string;
+};
 
 function App() {
   const [city, setCity] = useState<string>("");
+  const [results, setResults] = useState<ResultsStateType>({
+    country: "",
+    cityName: "",
+    temperature: "",
+    conditionText: "",
+    icon: "",
+  });
+
   const getWeather = (e: any) => {
     e.preventDefault();
     fetch(
-      "https://api.weatherapi.com/v1/current.json?key=c2fbbeb20a584e528b071710221112&q=London&aqi=no"
+      `https://api.weatherapi.com/v1/current.json?key=c2fbbeb20a584e528b071710221112&q=${city}&aqi=no`
     )
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        setResults({
+          country: data.location.country,
+          cityName: data.location.name,
+          temperature: data.current.temp_c,
+          conditionText: data.current.condition.text,
+          icon: data.current.condition.icon,
+        });
+      });
   };
+
   return (
     <div className="test">
-
       <Title />
       <Form setCity={setCity} getWeather={getWeather} />
-      <Results />
+      <Results results={results} />
     </div>
   );
 }
-
 export default App;
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,7 +1,28 @@
 // Results.tsx
 
-const Results = () => {
-  return <h1>気象データ</h1>;
+type ResultsPropsType = {
+  results: {
+    country: string;
+    cityName: string;
+    temperature: string;
+    conditionText: string;
+    icon: string;
+  };
+};
+
+const Results = (props: ResultsPropsType) => {
+  return (
+    <div>
+      {props.results.country && <div>{props.results.country}</div>}
+      {props.results.cityName && <div>{props.results.cityName}</div>}
+      {props.results.temperature && <div>{props.results.temperature}<span>°C</span></div>}
+      {props.results.conditionText &&  <div>
+          <img src={props.results.icon} alt="icon" />
+          <span>{props.results.conditionText}</span>
+        </div>
+      }
+    </div>
+  );
 };
 
 export default Results;
